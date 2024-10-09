84482912d3644f20d448085b8e60458768e74117
Emre Aygener <emre.aygener@gmail.com>
2024-10-09 16:37:00 +0300

    OOPWeek2

---
+++ BLM2005 - Nesne Yönelimli Programlama/Week2Deneme.java
@@ -0,0 +1,10 @@
+public class Week2Deneme {
+
+    public static void main(String[] args) {
+        java.util.Date date = new java.util.Date();
+        System.out.println("Current date and time is " + date);
+        System.out.println(new Circle());
+        System.out.println(new Circle());
+    }
+
+}
